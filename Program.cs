cd269e2818008adca58ade9df2b2faf526e37bc3
antorensh <antonellirenzo@outlook.com>
2021-09-03 00:51:59 +0200

    new .cs project for the program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhatsApp_Sticker_To_Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
